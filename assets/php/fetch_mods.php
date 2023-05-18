e53dd889416ec6ff353eebf20e1173e4467a8989
macinsight <gh@macinsight.net>
2023-05-18 22:30:33 +0200

    added dedicated page for mods

--- assets/php/fetch_mods.php
+++ assets/php/fetch_mods.php
@@ -11,15 +11,26 @@ $result = $conn->query($sql);
 
 // Generate HTML dynamically
 if ($result->num_rows > 0) {
-    echo '<table class="table table-hover table-sm">';
-    echo '<thead><tr><th>Mod Name</th><th>Required?</th></tr></thead>';
+    echo '<table class="table table-hover">';
+    echo '<thead><tr><th>Mod Name</th><th>File Size</th><th>Required?</th></tr></thead>';
     echo '<tbody>';
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
-        echo "<td><a class='btn btn-primary btn-sm' href='https://steamcommunity.com/sharedfiles/filedetails/?id=" . $row['mod_id'] . "' target='_blank'>" . $row['mod_name'] . "</a></td>";
+        echo "<td><a class='link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover' href='https://steamcommunity.com/sharedfiles/filedetails/?id=" . $row['mod_id'] . "'>" . $row['mod_name'] . "</a></td>";
+        
+        // Query Steam API for file size
+        $steamAPIKey = "198E9A22758D200BC3BF16C1A4FE9130";
+        $modID = $row['mod_id'];
+        $url = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/?key=$steamAPIKey&itemcount=1&publishedfileids[0]=$modID";
+        $json = file_get_contents($url);
+        $data = json_decode($json, true);
+        $fileSize = isset($data['response']['publishedfiledetails'][0]['file_size']) ? $data['response']['publishedfiledetails'][0]['file_size'] : 'N/A';
+        
+        echo "<td>" . $fileSize . "</td>";
+        
         echo "<td>";
         if ($row['mod_required'] == 1) {
-            echo '<span class="badge rounded-pill text-bg-info">Required</span>';
+            echo '<span class="badge rounded-pill text-success text-bg-info">Required</span>';
         } else {
             echo '<span class="badge rounded-pill text-secondary">Not Required</span>';
         }
