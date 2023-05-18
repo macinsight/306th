3b36c13b864d61f86074922aa99a8e6a969cd2d8
macinsight <gh@macinsight.net>
2023-05-18 17:33:08 +0200

    updated formedit

--- admin/formedit.php
+++ admin/formedit.php
@@ -1,8 +1,79 @@
+<?php
+// Database connection
+$conn = new mysqli("localhost:3306", "306_ops", "buG9*9x23!!", "operations");
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Check if the form is submitted for creating or updating a record
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['create'])) {
+        // Create a new record
+        $operationName = $_POST['operation_name'];
+        $date = $_POST['date'];
+        $time = $_POST['time'];
+        $location = $_POST['location'];
+        $description = $_POST['description'];
+
+        $sql = "INSERT INTO operations (operation_name, date, time, location, description) VALUES ('$operationName', '$date', '$time', '$location', '$description')";
+        if ($conn->query($sql) === true) {
+            echo "Record created successfully.";
+        } else {
+            echo "Error creating record: " . $conn->error;
+        }
+    } elseif (isset($_POST['update'])) {
+        // Update an existing record
+        $operationId = $_POST['operation_id'];
+        $operationName = $_POST['operation_name'];
+        $date = $_POST['date'];
+        $time = $_POST['time'];
+        $location = $_POST['location'];
+        $description = $_POST['description'];
+
+        $sql = "UPDATE operations SET operation_name='$operationName', date='$date', time='$time', location='$location', description='$description' WHERE id=$operationId";
+        if ($conn->query($sql) === true) {
+            echo "Record updated successfully.";
+        } else {
+            echo "Error updating record: " . $conn->error;
+        }
+    } elseif (isset($_POST['delete'])) {
+        // Delete a record
+        $operationId = $_POST['operation_id'];
+
+        $sql = "DELETE FROM operations WHERE id=$operationId";
+        if ($conn->query($sql) === true) {
+            echo "Record deleted successfully.";
+        } else {
+            echo "Error deleting record: " . $conn->error;
+        }
+    }
+}
+
+// Close the database connection
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-    <title>CRUD Operations</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+	<script src="/assets/js/color-modes.js"></script>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="github.com/macinsight">
+	<title>The 306th | Your next ArmA 3 unit</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	<link href="css/cover.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+	<meta property="og:title" content="The 306th Assault Brigade" />
+	<meta name="description"
+		content="The 306th is recruiting! Join the Discord for more info - 1980-1997 Bundeswehr Mechanized Arma 3 Unit.">
+	<meta property="og:url" content="https://306th.macinsight.net" />
+	<meta property="og:image" content="https://306th.macinsight.net/assets/img/306th.png" />
+	<meta name="keywords"
+		content="ArmA, ArmA 3, unit, milsim, Bundeswehr, German, semi-milsim, tactical combat, immersive, missions, camaraderie, milsim experience">
+	<meta name="theme-color" content="#4a5342">
 </head>
 <body>
     <div class="container">
@@ -11,145 +82,79 @@
         <h2>Create Record</h2>
         <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <div class="mb-3">
-                <label for="createName" class="form-label">Operation Name</label>
-                <input type="text" class="form-control" id="createName" name="createName" required>
+                <label for="operation_name" class="form-label">Operation Name:</label>
+                <input type="text" class="form-control" name="operation_name" required>
             </div>
+
             <div class="mb-3">
-                <label for="createDate" class="form-label">Date</label>
-                <input type="date" class="form-control" id="createDate" name="createDate" required>
+                <label for="date" class="form-label">Date:</label>
+                <input type="date" class="form-control" name="date" required>
             </div>
+
             <div class="mb-3">
-                <label for="createTime" class="form-label">Time</label>
-                <input type="time" class="form-control" id="createTime" name="createTime" required>
+                <label for="time" class="form-label">Time:</label>
+                <input type="time" class="form-control" name="time" required>
             </div>
+
             <div class="mb-3">
-                <label for="createLocation" class="form-label">Location</label>
-                <input type="text" class="form-control" id="createLocation" name="createLocation" required>
+                <label for="location" class="form-label">Location:</label>
+                <input type="text" class="form-control" name="location" required>
             </div>
+
             <div class="mb-3">
-                <label for="createDescription" class="form-label">Description</label>
-                <textarea class="form-control" id="createDescription" name="createDescription" rows="3" required></textarea>
+                <label for="description" class="form-label">Description:</label>
+                <textarea class="form-control" name="description" required></textarea>
             </div>
+
             <button type="submit" class="btn btn-primary" name="create">Create</button>
         </form>
 
         <h2>Update Record</h2>
         <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <div class="mb-3">
-                <label for="updateId" class="form-label">Operation ID</label>
-                <input type="text" class="form-control" id="updateId" name="updateId" required>
+                <label for="operation_id" class="form-label">Operation ID:</label>
+                <input type="text" class="form-control" name="operation_id" required>
             </div>
+
             <div class="mb-3">
-                <label for="updateName" class="form-label">Operation Name</label>
-                <input type="text" class="form-control" id="updateName" name="updateName" required>
+                <label for="operation_name" class="form-label">Operation Name:</label>
+                <input type="text" class="form-control" name="operation_name" required>
             </div>
+
             <div class="mb-3">
-                <label for="updateDate" class="form-label">Date</label>
-                <input type="date" class="form-control" id="updateDate" name="updateDate" required>
+                <label for="date" class="form-label">Date:</label>
+                <input type="date" class="form-control" name="date" required>
             </div>
+
             <div class="mb-3">
-                <label for="updateTime" class="form-label">Time</label>
-                <input type="time" class="form-control" id="updateTime" name="updateTime" required>
+                <label for="time" class="form-label">Time:</label>
+                <input type="time" class="form-control" name="time" required>
             </div>
+
             <div class="mb-3">
-                <label for="updateLocation" class="form-label">Location</label>
-                <input type="text" class="form-control" id="updateLocation" name="updateLocation" required>
+                <label for="location" class="form-label">Location:</label>
+                <input type="text" class="form-control" name="location" required>
             </div>
+
             <div class="mb-3">
-                <label for="updateDescription" class="form-label">Description</label>
-                <textarea class="form-control" id="updateDescription" name="updateDescription" rows="3" required></textarea>
+                <label for="description" class="form-label">Description:</label>
+                <textarea class="form-control" name="description" required></textarea>
             </div>
+
             <button type="submit" class="btn btn-primary" name="update">Update</button>
         </form>
 
         <h2>Delete Record</h2>
         <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
             <div class="mb-3">
-                <label for="deleteId" class="form-label">Operation ID</label>
-                <input type="text" class="form-control" id="deleteId" name="deleteId" required>
+                <label for="operation_id" class="form-label">Operation ID:</label>
+                <input type="text" class="form-control" name="operation_id" required>
             </div>
+
             <button type="submit" class="btn btn-danger" name="delete">Delete</button>
         </form>
-
-        <h2>All Operations</h2>
-        <button type="button" class="btn btn-primary" id="fetchOperationsBtn">Fetch All Operations</button>
-        <table class="table mt-3" id="operationsTable">
-            <thead>
-                <tr>
-                    <th>Operation ID</th>
-                    <th>Operation Name</th>
-                    <th>Date</th>
-                    <th>Time</th>
-                    <th>Location</th>
-                    <th>Description</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($operations as $operation) { ?>
-                <tr>
-                    <td><?php echo $operation['id']; ?></td>
-                    <td><?php echo $operation['operation_name']; ?></td>
-                    <td><?php echo $operation['date']; ?></td>
-                    <td><?php echo $operation['time']; ?></td>
-                    <td><?php echo $operation['location']; ?></td>
-                    <td><?php echo $operation['description']; ?></td>
-                </tr>
-                <?php } ?>
-            </tbody>
-        </table>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script>
-        // Fetch operations and update table on button click
-        document.getElementById('fetchOperationsBtn').addEventListener('click', function() {
-            fetchOperations();
-        });
-
-        function fetchOperations() {
-            // Send an AJAX request to fetch all operations
-            var xhr = new XMLHttpRequest();
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        // Parse the response and update the table
-                        var operations = JSON.parse(xhr.responseText);
-                        updateTable(operations);
-                    } else {
-                        console.error('Error fetching operations: ' + xhr.status);
-                    }
-                }
-            };
-            xhr.open('GET', 'fetch_operations.php', true);
-            xhr.send();
-        }
-
-        function updateTable(operations) {
-            var tableBody = document.getElementById('operationsTable').getElementsByTagName('tbody')[0];
-            tableBody.innerHTML = '';
-
-            operations.forEach(function(operation) {
-                var row = tableBody.insertRow();
-
-                var idCell = row.insertCell();
-                idCell.textContent = operation.id;
-
-                var nameCell = row.insertCell();
-                nameCell.textContent = operation.operation_name;
-
-                var dateCell = row.insertCell();
-                dateCell.textContent = operation.date;
-
-                var timeCell = row.insertCell();
-                timeCell.textContent = operation.time;
-
-                var locationCell = row.insertCell();
-                locationCell.textContent = operation.location;
-
-                var descriptionCell = row.insertCell();
-                descriptionCell.textContent = operation.description;
-            });
-        }
-    </script>
 </body>
 </html>
