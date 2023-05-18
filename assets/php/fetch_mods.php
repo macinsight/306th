bb085006692b88b8a70bf7f30ba7e7e561eb8e1a
macinsight <gh@macinsight.net>
2023-05-18 21:11:49 +0200

    reverted edit

--- assets/php/fetch_mods.php
+++ assets/php/fetch_mods.php
@@ -1,6 +1,4 @@
 <?php
-$steamApiKey = '198E9A22758D200BC3BF16C1A4FE9130';
-
 // Database connection
 $conn = new mysqli("localhost:3306", "306_ops", "buG9*9x23!!", "operations");
 if ($conn->connect_error) {
@@ -14,29 +12,20 @@ $result = $conn->query($sql);
 // Generate HTML dynamically
 if ($result->num_rows > 0) {
   echo '<table class="table table-hover">';
-  echo '<thead><tr><th>Mod Name</th><th>Steam URL</th><th>Required?</th><th>New Addition</th></tr></thead>';
+  echo '<thead><tr><th>Mod Name</th><th>Steam URL</th><th>Required?</th></tr></thead>';
   echo '<tbody>';
   while ($row = $result->fetch_assoc()) {
-    $modId = $row['mod_id'];
-    $steamUrl = "https://steamcommunity.com/sharedfiles/filedetails/?id=" . $modId;
-
-    // Get mod details from Steam Workshop API
-    $steamApiUrl = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/?itemcount=1&publishedfileids[0]=$modId&format=json&key=$steamApiKey";
-    $modDetails = json_decode(file_get_contents($steamApiUrl), true);
-
-    // Check if mod details are available
-    if (isset($modDetails['response']['publishedfiledetails'][0])) {
-      $modName = $modDetails['response']['publishedfiledetails'][0]['title'];
-      $modRequired = $row['mod_required'];
-
-      echo "<tr>";
-      echo "<td>$modName</td>";
-      echo "<td><a href='$steamUrl'>$modId</a></td>";
-      echo "<td>$modRequired</td>";
-      echo "<td>";
-      echo "</td>";
-      echo "</tr>";
+    echo "<tr>";
+    echo "<td>" . $row['mod_name'] . "</td>";
+    echo "<td><a class='link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover' href='https://steamcommunity.com/sharedfiles/filedetails/?id=" . $row['mod_id'] . "'>" . $row['mod_id'] . "</a></td>";
+    echo "<td>";
+    if ($row['mod_required'] == 1) {
+      echo '<span class="badge rounded-pill text-bg-info">Required</span>';
+    } else {
+      echo '<span class="badge rounded-pill text-secondary">Not Required</span>';
     }
+    echo "</td>";
+    echo "</tr>";
   }
   echo '</tbody>';
   echo '</table>';
