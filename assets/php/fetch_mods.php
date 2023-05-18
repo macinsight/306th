86c605b055aa690dd4b91bd3daa0220373810688
macinsight <gh@macinsight.net>
2023-05-18 22:07:22 +0200

    updated fetch_mods

--- assets/php/fetch_mods.php
+++ assets/php/fetch_mods.php
@@ -2,35 +2,37 @@
 // Database connection
 $conn = new mysqli("localhost:3306", "306_ops", "buG9*9x23!!", "operations");
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-// Query to fetch upcoming operations
+// Query to fetch mod data
 $sql = "SELECT * FROM modlist ORDER BY id ASC";
 $result = $conn->query($sql);
 
 // Generate HTML dynamically
 if ($result->num_rows > 0) {
-  echo '<table class="table table-hover">';
-  echo '<thead><tr><th>Mod Name</th><th>Steam URL</th><th>Required?</th></tr></thead>';
-  echo '<tbody>';
-  while ($row = $result->fetch_assoc()) {
-    echo "<tr>";
-    echo "<td>" . $row['mod_name'] . "</td>";
-    echo "<td><a class='link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover' href='https://steamcommunity.com/sharedfiles/filedetails/?id=" . $row['mod_id'] . "'>" . $row['mod_id'] . "</a></td>";
-    echo "<td>";
-    if ($row['mod_required'] == 1) {
-      echo '<span class="badge rounded-pill text-bg-info">Required</span>';
-    } else {
-      echo '<span class="badge rounded-pill text-secondary">Not Required</span>';
+    echo '<div class="container pt-5">';
+    echo '<h3 class="pb-3">Our Mods</h3>';
+    echo '<table class="table table-hover">';
+    echo '<thead><tr><th>Mod Name</th><th>Required?</th></tr></thead>';
+    echo '<tbody>';
+    while ($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td><a class='btn btn-link' href='https://steamcommunity.com/sharedfiles/filedetails/?id=" . $row['mod_id'] . "' target='_blank'>" . $row['mod_name'] . "</a></td>";
+        echo "<td>";
+        if ($row['mod_required'] == 1) {
+            echo '<span class="badge rounded-pill text-bg-info">Required</span>';
+        } else {
+            echo '<span class="badge rounded-pill text-secondary">Not Required</span>';
+        }
+        echo "</td>";
+        echo "</tr>";
     }
-    echo "</td>";
-    echo "</tr>";
-  }
-  echo '</tbody>';
-  echo '</table>';
+    echo '</tbody>';
+    echo '</table>';
+    echo '</div>';
 } else {
-  echo "<p>No Mods found.</p>";
+    echo "<p>No Mods found.</p>";
 }
 
 // Close the database connection
