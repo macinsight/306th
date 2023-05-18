dfcb11f547794cb153739045b8de02dcad5a64cb
macinsight <gh@macinsight.net>
2023-05-18 16:33:31 +0200

    updated fetch_operations

--- fetch_operations.php
+++ fetch_operations.php
@@ -14,10 +14,10 @@ if ($result->num_rows > 0) {
   while ($row = $result->fetch_assoc()) {
     echo "<div class='operation'>";
     echo "<h3>" . $row['operation_name'] . "</h3>";
-    echo "<p>Date: " . $row['date'] . "</p>";
-    echo "<p>Time: " . $row['time'] . "</p>";
-    echo "<p>Location: " . $row['location'] . "</p>";
-    echo "<p>Description: " . $row['description'] . "</p>";
+    echo "<p><span class="bi-calendar2-event" width="32" height="32"></span>: " . $row['date'] . "</p>";
+    echo "<p><span class="bi-stopwatch" width="32" height="32"></span> " . $row['time'] . "</p>";
+    echo "<p><span class="bi-geo-alt" width="32" height="32"></span>" . $row['location'] . "</p>";
+    echo "<p><span class="bi-card-text" width="32" height="32"></span> " . $row['description'] . "</p>";
     echo "</div>";
   }
 } else {
