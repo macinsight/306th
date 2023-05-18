0f386865ccf4a03521a52dba2ce6f721cb416304
macinsight <gh@macinsight.net>
2023-05-18 17:00:25 +0200

    Made table use users default timezone

--- fetch_operations.php
+++ fetch_operations.php
@@ -5,6 +5,9 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+// Set the default timezone
+date_default_timezone_set('UTC');
+
 // Query to fetch upcoming operations
 $sql = "SELECT * FROM operations ORDER BY date ASC";
 $result = $conn->query($sql);
@@ -12,13 +15,18 @@ $result = $conn->query($sql);
 // Generate HTML dynamically
 if ($result->num_rows > 0) {
   echo '<table class="table table-striped">';
-  echo '<thead><tr><th>Operation Name</th><th><i class="bi bi-calendar2-event"></i> Date</th><th><i class="bi bi-stopwatch"></i> Time</th><th><i class="bi bi-geo-alt"></i> Location</th><th><i class="bi bi-card-text"></i> Description</th></tr></thead>';
+  echo '<thead><tr><th>Operation Name</th><th><i class="bi bi-calendar2-event"></i> Date</th><th><i class="bi bi-stopwatch"></i> Time (Local)</th><th><i class="bi bi-geo-alt"></i> Location</th><th><i class="bi bi-card-text"></i> Description</th></tr></thead>';
   echo '<tbody>';
   while ($row = $result->fetch_assoc()) {
     echo "<tr>";
     echo "<td>" . $row['operation_name'] . "</td>";
     echo "<td>" . $row['date'] . "</td>";
-    echo "<td>" . $row['time'] . "</td>";
+
+    // Convert time to user's local time
+    $time = new DateTime($row['time'], new DateTimeZone('UTC'));
+    $time->setTimezone(new DateTimeZone(date_default_timezone_get()));
+    echo "<td>" . $time->format('H:i') . "</td>";
+
     echo "<td>" . $row['location'] . "</td>";
     echo "<td>" . $row['description'] . "</td>";
     echo "</tr>";
