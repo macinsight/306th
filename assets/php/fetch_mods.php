a39acb03f18fa9ced6ad58e0f0f536b54467e540
macinsight <gh@macinsight.net>
2023-05-18 21:58:29 +0200

    updated fetch_mods

--- assets/php/fetch_mods.php
+++ assets/php/fetch_mods.php
@@ -2,38 +2,35 @@
 // Database connection
 $conn = new mysqli("localhost:3306", "306_ops", "buG9*9x23!!", "operations");
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+  die("Connection failed: " . $conn->connect_error);
 }
 
-// Query to fetch mod data
+// Query to fetch upcoming operations
 $sql = "SELECT * FROM modlist ORDER BY id ASC";
 $result = $conn->query($sql);
 
 // Generate HTML dynamically
 if ($result->num_rows > 0) {
-    echo '<div class="accordion" id="modAccordion">';
-    while ($row = $result->fetch_assoc()) {
-        echo '<div class="accordion-item">';
-        echo '<h2 class="accordion-header" id="modHeading' . $row['id'] . '">';
-        echo '<button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#modCollapse' . $row['id'] . '" aria-expanded="true" aria-controls="modCollapse' . $row['id'] . '">';
-        echo $row['mod_name'];
-        echo '</button>';
-        echo '</h2>';
-        echo '<div id="modCollapse' . $row['id'] . '" class="accordion-collapse collapse" aria-labelledby="modHeading' . $row['id'] . '" data-bs-parent="#modAccordion">';
-        echo '<div class="accordion-body">';
-        echo '<p>Steam URL: <a href="https://steamcommunity.com/sharedfiles/filedetails/?id=' . $row['mod_id'] . '">' . $row['mod_id'] . '</a></p>';
-        if ($row['mod_required'] == 1) {
-            echo '<span class="badge rounded-pill text-bg-info">Required</span>';
-        } else {
-            echo '<span class="badge rounded-pill text-secondary">Not Required</span>';
-        }
-        echo '</div>';
-        echo '</div>';
-        echo '</div>';
+  echo '<table class="table table-hover">';
+  echo '<thead><tr><th>Mod Name</th><th>Steam URL</th><th>Required?</th></tr></thead>';
+  echo '<tbody>';
+  while ($row = $result->fetch_assoc()) {
+    echo "<tr>";
+    echo "<td>" . $row['mod_name'] . "</td>";
+    echo "<td><a class='link-offset-2 link-offset-3-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover' href='https://steamcommunity.com/sharedfiles/filedetails/?id=" . $row['mod_id'] . "'>" . $row['mod_id'] . "</a></td>";
+    echo "<td>";
+    if ($row['mod_required'] == 1) {
+      echo '<span class="badge rounded-pill text-bg-info">Required</span>';
+    } else {
+      echo '<span class="badge rounded-pill text-secondary">Not Required</span>';
     }
-    echo '</div>';
+    echo "</td>";
+    echo "</tr>";
+  }
+  echo '</tbody>';
+  echo '</table>';
 } else {
-    echo "<p>No Mods found.</p>";
+  echo "<p>No Mods found.</p>";
 }
 
 // Close the database connection
