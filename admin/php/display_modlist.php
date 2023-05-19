c3b4c8044ea0dd7c5466e76be0962db9825c5540
macinsight <gh@macinsight.net>
2023-05-19 04:36:34 +0200

    update

--- admin/php/display_modlist.php
+++ admin/php/display_modlist.php
@@ -123,10 +123,10 @@ if ($result->num_rows > 0) {
         echo "</td>";
         echo "<td>";
         echo "<div class='form-check form-switch'>";
-        echo "<input class='form-check-input' type='checkbox' role='switch' id="switch_$modID">"
-        echo "<label class='form-check-label' for='switch_$modID'>Required</label>"
+        echo "<input class='form-check-input' type='checkbox' role='switch' id='switch_$modID'" . ($row['mod_required'] == 1 ? ' checked' : '') . ">";
+        echo "<label class='form-check-label' for='switch_$modID'>Required</label>";
         echo "</div>";
-        echo "<td>"
+        echo "<td>";
         echo "</tr>";
     }
     echo '</tbody>';
