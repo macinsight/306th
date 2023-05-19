1c711193de03bc8e56f088bf57ff472e7740d8c1
macinsight <gh@macinsight.net>
2023-05-19 18:28:57 +0200

    things

--- admin/display.php
+++ admin/display.php
@@ -1,20 +1,20 @@
 <?php
 
-function displayHeaderContent()
+function displayHead()
 {
+    echo '
     <head>
-	<script src="/assets/js/color-modes.js"></script>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="author" content="github.com/macinsight">
-	<title>The 306th | Your next ArmA 3 unit</title>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-	<link href="css/cover.css" rel="stylesheet">
-	<link href="css/custom.css" rel="stylesheet">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-</head>
-}
+        <script src="/assets/js/color-modes.js"></script>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="github.com/macinsight">
+        <title>The 306th | Your next ArmA 3 unit</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        <link href="css/cover.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    </head>';
 function displayThemeSelector()
 {
     echo '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">';
