2384c0d5fee326f0532398ab27842821fb4d29bf
macinsight <gh@macinsight.net>
2023-05-19 07:36:05 +0200

    fixing things?

--- admin/index.php
+++ admin/index.php
@@ -179,7 +179,24 @@
 			</ul>
 		</footer>
 	</div>
-	
+
+	<script>
+		function submitForm() {
+			var form = document.getElementById('modForm');
+			var formData = new FormData(form);
+
+			var xhr = new XMLHttpRequest();
+			xhr.open('POST', window.location.href);
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+					// Reload the mod list content only
+					var modList = document.getElementById('modList');
+					modList.innerHTML = xhr.responseText;
+				}
+			};
+			xhr.send(formData);
+		}
+	</script>
 
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
 		integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
