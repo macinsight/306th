18a38a221cc97c71695b767343a8b0967cdcfb45
macinsight <gh@macinsight.net>
2023-05-19 03:13:39 +0200

    Fixed link to stylesheet

--- admin/index.php
+++ admin/index.php
@@ -9,7 +9,7 @@
 	<title>306 - Admin Panel</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-	<link href="css/custom.css" rel="stylesheet">
+	<link href="../css/custom.css" rel="stylesheet">
 </head>
 
 <body data-bs-spy="scroll" data-bs-target="#navbar">
@@ -87,7 +87,6 @@
 			<span class="bi-terminal width=" 48" height="48""></span>
         <span class="fs-4">Admin Panel!</span>
       </a>
-
       <ul class="nav nav-pills">
         <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
         <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
