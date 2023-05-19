bc1de9e994bec388aa0fc08d973e9756b40b88a0
macinsight <gh@macinsight.net>
2023-05-19 03:44:28 +0200

    Added dropdown.

--- admin/index.php
+++ admin/index.php
@@ -115,31 +115,49 @@
 	</svg>
 
 	<div class="container sticky-top" id="navbar">
-		<nav class="navbar navbar-expand-lg bg-body-tertiary rounded" id="navigator">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="#">306th Assault Brigade</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbartoggler"
-					aria-controls="navbartoggler" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbartoggler">
-					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
-						<a href="#intro" class="nav-link nav-item">Home</a>
-						<a href="#images" class="nav-item nav-link px-2">Gallery</a>
-						<a href="#about" class="nav-item nav-link px-2">About</a>
-						<a href="#upcoming-operations" class="nav-item nav-link px-2">Upcoming Ops</a>
-						<a href="#units" class="nav-item nav-link px-2">Units</a>
-						<a href="#values" class="nav-item nav-link px-2">Values</a>
-					</ul>
-					<div class="d-lg-flex col-lg-3 justify-content-lg-end">
-						<a class="icon-link" href="https://discord.gg/D3U3e9seZ7">
-							<button type="submit" class="btn btn-primary"><span class="bi-discord width=" 48" height="48""></span></button>
-						</a>
-					</div>
-				</div>
-			</div>
-		</nav>
-	</div>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary rounded" id="navigator">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">306th Assault Brigade</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbartoggler"
+        aria-controls="navbartoggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbartoggler">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a href="#intro" class="nav-link">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="operationsDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Operations
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="operationsDropdown">
+              <li><a class="dropdown-item" href="#">Create</a></li>
+              <li><a class="dropdown-item" href="#">Update</a></li>
+              <li><a class="dropdown-item" href="#">Delete 3</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="modlistDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Modlist
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="modlistDropdown">
+              <li><a class="dropdown-item" href="#">Create</a></li>
+              <li><a class="dropdown-item" href="#">Update</a></li>
+              <li><a class="dropdown-item" href="#">Delete</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</div>
+
+
+
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+						integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+						crossorigin="anonymous"></script>
 
 </body>
 
