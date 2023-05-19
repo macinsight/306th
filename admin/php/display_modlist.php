d8710bd6d6bd8d1d0d64e2615557142a5ae7ef1f
macinsight <gh@macinsight.net>
2023-05-19 05:03:20 +0200

    Added deletion checkbox

--- admin/php/display_modlist.php
+++ admin/php/display_modlist.php
@@ -77,14 +77,14 @@ if ($result->num_rows > 0) {
     echo '<form method="POST">'; // Start the form
 
     echo '<table class="table table-hover">';
-    echo '<thead><tr><th>Mod Name</th><th>File Size (MB)</th><th>Required?</th></tr></thead>';
+    echo '<thead><tr><th>Delete</th><th>Mod Name</th><th>File Size (MB)</th><th>Required?</th></tr></thead>';
     echo '<tbody>';
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         $modID = $row['mod_id'];
 
         // Define cache file path
-        $cacheFile = "../../cache/$modID.cache";
+        $cacheFile = "cache/$modID.cache";
 
         // Check if API response is available in cache and not expired
         $response = retrieveAPIResponseFromCache($cacheFile, $cacheDuration);
@@ -112,11 +112,18 @@ if ($result->num_rows > 0) {
             $fileSizeBytes = isset($fileDetails['file_size']) ? $fileDetails['file_size'] : 0;
             $fileSizeMB = round($fileSizeBytes / (1024 * 1024), 2);
 
-            // Output the link with the mod title
+            // Output the checkbox, mod title, and other mod details
+            echo "<td>";
+            echo "<div class='form-check'>";
+            echo "<input class='form-check-input' type='checkbox' id='checkbox_$modID' name='delete_mod[]' value='$modID'>";
+            echo "<label class='form-check-label' for='checkbox_$modID'></label>";
+            echo "</div>";
+            echo "</td>";
             echo "<td><a href='https://steamcommunity.com/sharedfiles/filedetails/?id=$modID' class='link-offset-2 link-offset-2-hover link-underline link-underline-opacity-0 link-underline-opacity-75-hover' target='_blank'>$modTitle</a></td>";
             echo "<td>$fileSizeMB MB</td>";
         } else {
             // Output "N/A" if mod details are not available
+            echo "<td></td>";
             echo "<td>N/A</td>";
             echo "<td>N/A</td>";
         }
@@ -145,6 +152,12 @@ if ($result->num_rows > 0) {
             updateModRequiredStatus($modID, $required);
         }
 
+        $deleteMods = isset($_POST['delete_mod']) ? $_POST['delete_mod'] : [];
+
+        // Prepare the record for deletion
+        $deleteSql = "UPDATE modlist SET to_be_deleted = 1 WHERE mod_id IN ('" . implode("','", $deleteMods) . "')";
+        $conn->query($deleteSql);
+
         // Refresh the page after updating the database
         header('Location: ' . $_SERVER['PHP_SELF']);
         exit();
@@ -155,4 +168,5 @@ if ($result->num_rows > 0) {
 } else {
     echo "<p>No Mods found.</p>";
 }
+
 ?>
