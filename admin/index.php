32158f7f45a2bc31223f9df06e5cc024d92ec827
macinsight <gh@macinsight.net>
2023-05-19 18:18:54 +0200

    added index selector with js

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,7 @@
 <?php
+
 require_once 'display.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -22,9 +24,18 @@ require_once 'display.php';
 	<?php
 	displayThemeSelector();
 	displayNavbar();
+	
+
+	// Check condition and include the corresponding file
+	if ($condition) {
+		include 'modlist.php';
+	} else {
+		include 'path/to/operations.php';
+	}
+
 	displayFooter();
 	?>
-
+	<script src="indexselector.js"></script>
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
 		integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
 		crossorigin="anonymous"></script>
