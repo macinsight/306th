e5d93297f333c836b17c12ff57c593f8355e0f5b
macinsight <gh@macinsight.net>
2023-05-19 16:03:11 +0200

    changed things

--- admin/index.php
+++ admin/index.php
@@ -3,6 +3,7 @@
 require_once 'config.php';
 require_once 'functions.php';
 require_once 'submit.php';
+require_once 'display.php';
 
 ?>
 
@@ -24,7 +25,13 @@ require_once 'submit.php';
 
 <body>
     <div class="container">
-        <?php require_once 'modlist.php'; ?>
+        <?php
+        // Call the displayNavbar function
+        displayNavbar();
+
+        // Call the displayFooter function
+        displayFooter();
+        ?>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
